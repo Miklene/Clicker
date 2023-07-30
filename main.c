4ab4fd66aee20d2798db6d383fee2704b1f06dd0
Kotov Mikhail <kotov-mikhail@bk.ru>
2023-07-30 14:26:28 +0300

    Создал h файл для Main_window. Раскидал код по функциям

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h> 
 #include <Windows.h> 
 #include <WinUser.h> 
+#include "main_window.h"
 
-LRESULT WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
-{
-	if (message == WM_DESTROY)
-		PostQuitMessage(0);
-	return DefWindowProcA(hwnd, message, wparam, lparam);
-}
-const wchar_t CLASS_NAME[] = L"Sample Window Class";
-const wchar_t WINDOW_NAME[] = L"Clicker";
 
 int main(void)
 {
@@ -18,33 +11,18 @@ int main(void)
 	double y_resolution = 65535 / 1080;
 	int x = 100 * x_resolution;
 	int y = 100 * y_resolution;
-	HINSTANCE hinstance = GetModuleHandleW(NULL);
-	//WinMain(hinstance, NULL, GetCommandLineW(), 0);
+
+	MainWindow_Init();
+	while (1)
+	{
+		MainWindow_Cycle();
+	}
+
 	/*char mes = getchar();
 	mouse_event(MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE, x, y, 0, 0);
 	mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
 	mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);*/
 
-	WNDCLASS wcl;
-	memset(&wcl, 0, sizeof(WNDCLASS));
-	wcl.lpfnWndProc = DefWindowProc;
-	wcl.hInstance = hinstance;
-	wcl.lpszClassName = CLASS_NAME;
-	//wcl.lpfnWndProc = DefWindowProcA;
-	RegisterClass(&wcl);
-
-	HWND hwnd;
-	hwnd = CreateWindowEx(0, CLASS_NAME, WINDOW_NAME, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hinstance, NULL);
-	if (hwnd == NULL)
-	{
-		return 0;
-	}
-	ShowWindow(hwnd, SW_SHOWNORMAL);
-
-	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		DispatchMessage(&msg);
-	}
+	
 	return 0;
 }
---
+++ main_window.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+void MainWindow_Init(void);
+void MainWindow_Cycle(void);
+
+#endif
